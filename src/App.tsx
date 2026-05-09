24fbcdf2a43411e0bef5208ad84a994f3a592e48
Cici20BAR <165190059+Cici20BAR@users.noreply.github.com>
2026-05-09 16:12:18 +0300

    Button logic+style

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
+import { Button } from "./ui/Button"
 function App() {
   return (
     // bg-slate-950 = fundalul închis din pozele tale
     // flex, items-center, justify-center = pune textul fix în mijloc
     <div className="min-h-screen bg-slate-950 flex items-center justify-center p-4">
-      <div className="bg-blue-600 text-white p-8 rounded-2xl shadow-2xl scale-110">
-        <h1 className="text-4xl font-black uppercase tracking-tighter">
-          Tailwind e activ! 🚀
-        </h1>
-        <p className="mt-2 text-blue-100 font-medium">
-          Dacă vezi acest card albastru pe fundal negru, poți începe designul.
-        </p>
-      </div>
+        
+      
+            <Button  variant= {"primary"}isActive={false} isAdd={false} >Apasa</Button>
+
+      
     </div>
   )
 }
---
+++ src/ui/Button.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
+    variant?:'primary'|'ghost'|'secondary' |'destroy',
+    isActive?:boolean,
+    isAdd?:boolean,
+}
+export function Button({
+    children,
+    variant='primary',
+    className='',
+    isActive=false,
+    isAdd=false,
+    ...props
+}:ButtonProps){
+    const baseStyle="flex items-center justify-center px-4 py-4 rounded-xl transition-all duration-200 active:scale-95 disabled:opacity-50 disabled:cursor-not-allowed. ";
+    const variants={
+        primary:"bg-blue-600 text-white shadow-md hover:bg-blue-300",
+        ghost:"bg-transparent text-slate-400 hover:bg-slate-800/50 hover:text-slate-200",
+        destroy:"bg-red-600/10 text-red-500 hover:bg-red-600 hover:text-white",
+        secondary: "bg-slate-700 text-slate-200 hover:bg-slate-600"
+    };
+    const finalVar=isActive?variants.primary:variants[variant];
+    const WidthStyle=isAdd? "w-full" : "w-auto";
+
+
+
+    return(
+        <button
+        className={`${baseStyle} ${finalVar} ${WidthStyle} ${className}`}
+        {...props}
+        >{children}</button>
+
+    );
+
+}
